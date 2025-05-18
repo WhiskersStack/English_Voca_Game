de78d8d87a5a15897bf859cd542f7256a06b67fa
Jen <ssystem32@gmail.com>
2025-05-18 13:46:27 +0300

    Add vocabulary data and initialize counters in x.json; update load_words function

--- training.py
+++ training.py
@@ -21,6 +21,12 @@ def load_words(unit, word1, word2):
     with open("vocabulary.json", "r", encoding="utf-8") as file:
         words_list = json.load(file)
 
+    for u in words_list:
+        for word in words_list[u]:
+            word['counter'] = 0
+
+    with open("x.json", "w", encoding="utf-8") as file:
+        json.dump(words_list, file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     load_words(1, "word1", "word2")
